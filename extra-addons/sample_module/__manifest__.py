5c3b0067ca68e11f39492d1de3718b5eb32487c3
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2024-07-05 00:16:20 +0800

    chore: change addons/ into extra-addons/

---
+++ extra-addons/sample_module/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Sample Module',
+    'summary': 'A sample module for demonstration purposes.',
+    'depends': [
+        'base'
+    ],
+    'data': [],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3'
+}
